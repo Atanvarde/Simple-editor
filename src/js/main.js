590e7cd9bb414ff9879302caa87bc6a881082dee
Aleksandra Gardo <aleksandra.gardo@gmail.com>
2019-07-29 13:24:32 +0200

    feat: local storage

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -16,7 +16,21 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const buttonLoad = document.querySelector(".editor__button--load-js");
 
-console.log(`Hello world!`);
+const buttonSave = document.querySelector(".editor__button--save-js");
+
+const textarea = document.querySelector(".editor__textarea--js");
+
+buttonSave.addEventListener('click', () => {
+  localStorage.setItem('textwritten', textarea.value);
+});
+
+buttonLoad.addEventListener('click', () => {
+  textarea.value = localStorage.getItem('textwritten');
+});
+
+localStorage.removeItem('textwritten');
+
 
 
